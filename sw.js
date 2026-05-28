6bad2b627113ba7eb0884414c70c71c53ed9ee32
Joseph Tsala <josephtsalase@email.com>
2026-05-28 13:37:46 +0100

    add files robot et sitemap

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'tinda-v1';
+const urlsToCache = [
+    '/',
+    '/shop.html',
+    '/cart.html',
+    '/checkout.html',
+    '/faqs.html',
+    '/conditions.html',
+    '/help.html',
+    '/login.html',
+    '/responsive.css',
+    '/responsive.js',
+    '/dashboard.html',
+    '/product.html',
+    '/categories.html',
+    'assets/icons/icone phone.svg',
+    'assets/icons/logo.svg'
+];
+
+// Installation
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+            .then(() => self.skipWaiting())
+    );
+});
+
+// Activation
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+// Stratégie : Network First, puis cache
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            })
+            .catch(() => caches.match(event.request))
+    );
+});
